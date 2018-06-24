2f6d7adcd5cb2616e142cf49ea475f18727b13bb
Aquerr <nerdianin@gmail.com>
2018-06-25 00:11:34 +0200

    Prevent dropping block that chest is hiding under

--- src/main/java/io/github/aquerr/chestrefill/listeners/ContainerBreakListener.java
+++ src/main/java/io/github/aquerr/chestrefill/listeners/ContainerBreakListener.java
@@ -1,6 +1,7 @@
 package io.github.aquerr.chestrefill.listeners;
 
 import io.github.aquerr.chestrefill.entities.ContainerLocation;
+import io.github.aquerr.chestrefill.entities.RefillableContainer;
 import io.github.aquerr.chestrefill.managers.ContainerManager;
 import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.data.Transaction;
@@ -8,12 +9,18 @@ import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.event.item.inventory.DropItemEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by Aquerr on 2018-02-16.
  */
 
 public class ContainerBreakListener
 {
+    private List<RefillableContainer> DESTROYED_CONTAINERS = new ArrayList<>(1);
+
     @Listener
     public void onRefillableEntityBreak(ChangeBlockEvent.Break event)
     {
@@ -23,12 +30,36 @@ public class ContainerBreakListener
             {
                 ContainerLocation containerLocation = new ContainerLocation(transaction.getOriginal().getPosition(), transaction.getOriginal().getWorldUniqueId());
 
-                if(ContainerManager.getRefillableContainers().stream().anyMatch(x->x.getContainerLocation().equals(containerLocation)))
+                for (RefillableContainer refillableContainer : ContainerManager.getRefillableContainers())
                 {
-                    //TODO: If player destroyed the container, inform him/she about it.
-                    ContainerManager.removeRefillableContainer(containerLocation);
+                    if (refillableContainer.getContainerLocation().equals(containerLocation))
+                    {
+                        //TODO: If player destroyed the container, inform him/she about it.
+                        DESTROYED_CONTAINERS.add(refillableContainer);
+                        ContainerManager.removeRefillableContainer(containerLocation);
+                        break;
+                    }
                 }
             }
         }
     }
+
+        @Listener
+        public void onItemDrop(DropItemEvent event)
+        {
+            Optional<BlockSnapshot> blockSnapshot = event.getCause().first(BlockSnapshot.class);
+
+            if (blockSnapshot.isPresent() && blockSnapshot.get().getLocation().isPresent())
+            {
+                ContainerLocation containerLocation = new ContainerLocation(blockSnapshot.get().getLocation().get().getBlockPosition(), blockSnapshot.get().getWorldUniqueId());
+
+                for (RefillableContainer refillableContainer : DESTROYED_CONTAINERS)
+                {
+                    if (refillableContainer.getContainerLocation().equals(containerLocation) && refillableContainer.getHidingBlock().equals(blockSnapshot.get().getState().getType()))
+                    {
+                        event.setCancelled(true);
+                    }
+                }
+            }
+        }
 }
