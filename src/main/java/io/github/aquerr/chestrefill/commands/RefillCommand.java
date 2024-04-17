8a10feb634144eb638265bcb7377042eec4e5cd5
Aquerr <nerdianin@gmail.com>
2024-04-17 13:55:35 +0200

    Fix RefillCommand not handling RefillableContainer argument

--- src/main/java/io/github/aquerr/chestrefill/commands/RefillCommand.java
+++ src/main/java/io/github/aquerr/chestrefill/commands/RefillCommand.java
@@ -1,14 +1,12 @@
 package io.github.aquerr.chestrefill.commands;
 
 import io.github.aquerr.chestrefill.ChestRefill;
+import io.github.aquerr.chestrefill.commands.arguments.ChestRefillCommandParameters;
 import io.github.aquerr.chestrefill.entities.RefillableContainer;
 import io.github.aquerr.chestrefill.messaging.MessageSource;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.exception.CommandException;
 import org.spongepowered.api.command.parameter.CommandContext;
-import org.spongepowered.api.command.parameter.Parameter;
-
-import java.util.Collection;
 
 public class RefillCommand extends AbstractCommand
 {
@@ -23,25 +21,16 @@ public class RefillCommand extends AbstractCommand
     @Override
     public CommandResult execute(CommandContext context) throws CommandException
     {
-        String containerName = context.requireOne(Parameter.string().key("name").build());
-        Collection<RefillableContainer> containers = super.getPlugin().getContainerManager().getRefillableContainers();
-
-        for (RefillableContainer refillableContainer : containers)
+        RefillableContainer refillableContainer = context.requireOne(ChestRefillCommandParameters.refillableContainer());
+        boolean didSucceed = super.getPlugin().getContainerManager().refillContainer(refillableContainer.getContainerLocation());
+        if(didSucceed)
         {
-            if (refillableContainer.getName() != null && refillableContainer.getName().equals(containerName))
-            {
-                boolean didSucceed = super.getPlugin().getContainerManager().refillContainer(refillableContainer.getContainerLocation());
-                if(didSucceed)
-                {
-                    context.cause().audience().sendMessage(messageSource.resolveMessageWithPrefix("command.refill.success"));
-                    break;
-                }
-                else
-                {
-                    throw messageSource.resolveExceptionWithMessage("command.refill.failure");
-                }
-            }
+            context.cause().audience().sendMessage(messageSource.resolveMessageWithPrefix("command.refill.success"));
+            return CommandResult.success();
         }
-        return CommandResult.success();
+        else
+        {
+            throw messageSource.resolveExceptionWithMessage("command.refill.failure");
+        }
     }
 }
